f1a310b6ecd3277f91e886f7f21cac19559ed107
marioamandio <30850605+marioamandio@users.noreply.github.com>
2018-03-22 15:45:29 +0000

    Init commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000
+
+const app = express();
+
+app.use(express.static(publicPath));
+
+
+//LISTEN
+app.listen(port, () => {
+    console.log('app is up on ' + port);
+})
