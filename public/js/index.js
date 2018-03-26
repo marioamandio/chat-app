2511bb93bc9aea22f2bb25d615a5f25b1b0d9fab
marioamandio <30850605+marioamandio@users.noreply.github.com>
2018-03-26 09:44:01 +0100

    add css for chst page

--- public/js/index.js
+++ public/js/index.js
@@ -28,14 +28,17 @@ socket.on('newLocationMessage', function(message) {
     jQuery('#messages').append(li);
 });
 
+
 jQuery('#message-form').on('click', function (ev) {
     ev.preventDefault();
 
+    var messageTextBox = jQuery('[name=message')
+
     socket.emit('createMessage', {
         from: 'User',
-        text: jQuery('[name=message').val()
+        text: messageTextBox.val()
     }, function () {
-
+        messageTextBox.val('')
     })
 });
 
@@ -46,12 +49,16 @@ locationButton.on("click", function () {
         return alert('geolocation not supported by your browser.')
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...')
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location...')
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
     }, function () {
+        locationButton.removeAttr('disabled');
         alert('Unable to fetch location')
     });
 
